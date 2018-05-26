2aa5e58653db196102b0e24ff7d7ee1a02315713
Pablo Salgado <pablo.salgado@iungoeducation.com>
2018-05-26 10:10:56 -0500

    Completa la programación del sistema de alarma

--- unad-mm-paso-6-programa-1.ino
+++ unad-mm-paso-6-programa-1.ino
@@ -17,9 +17,16 @@ int pTSZ2 = 9;  // Touch Sensor
 int pPSZ2 = 8;  // Pressure sensor
 
 // Definición de puertos para control de los LED
-int pLV = 7;
-int pLA = 6;
-int pLR = 5;
+int pLV = 7; // Verde
+int pLA = 6; // Amarillo
+int pLR = 5; // Rojo
+
+// Definición de puertos para el control de actuadores
+int pMZ1 = 4; // Motor de la puerta de la ZONA 1
+int pMZ2 = 3; // Motor de la puerta de la ZONA 2
+int pMPP = 2; // Motor de la puerta principal del museo
+int pEL  = 1; // Iluminación de emergencia
+int pBUZ = 0; // Sirena de la alarma
 
 boolean alarmaZ2;
 boolean alarmaZ1;
@@ -27,7 +34,7 @@ boolean alarmaZ1;
 void setup() {
   alarmaZ1 = false;
   alarmaZ2 = false;
-  
+
   // Configura puertos de la zona 1
   pinMode(pIFZ1, INPUT);
   pinMode(pTSZ1, INPUT);
@@ -38,8 +45,23 @@ void setup() {
   pinMode(pLA, OUTPUT);
   pinMode(pLR, OUTPUT);
 
+  // Puertos de control de los actuadores
+  pinMode(pMZ1, OUTPUT);
+  pinMode(pMZ2, OUTPUT);
+  pinMode(pMPP, OUTPUT);
+  pinMode(pEL, OUTPUT);
+  pinMode(pBUZ, OUTPUT);
+  
+  // Asegurar el estado inicial de los actuadores
+  digitalWrite(pMZ1, LOW);
+  digitalWrite(pMZ2, LOW);
+  digitalWrite(pMPP, LOW);
+  digitalWrite(pEL, LOW);
+  digitalWrite(pBUZ, LOW);
+
   lcd.begin();
   lcd.print("PASM Security");
+  
   delay(300);
 }
 
@@ -95,7 +117,7 @@ void loop() {
   // mensaje en el display, lo cual habilita el monitoreo de las restantes 
   // zonas para intentar evitar una distracción en una zona mientras el objetivo
   // es otra zona del museo.
-  if (!alarmaZ1) {
+  if (!alarmaZ2) {
     visualizarEstado("ZONA 2", lVZ2, lAZ2, lRZ2);
   } else {
     visualizarEstado("ZONA 2", false, false, true);
@@ -133,6 +155,11 @@ void visualizarEstado(const char msg[], boolean lv, boolean la, boolean lr) {
  * Rutina de activación de los actuadores en caso de alarma
  */
 void activarActuadores() {
-
+  // Activar los motores de las puertas de todas las zonas
+  digitalWrite(pMZ1, HIGH);
+  digitalWrite(pMZ2, HIGH);
+  digitalWrite(pMPP, HIGH);
+  digitalWrite(pEL, HIGH);
+  digitalWrite(pBUZ, HIGH);
 }
 
